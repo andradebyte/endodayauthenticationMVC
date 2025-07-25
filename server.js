80bfb60adf796de4b130a18cf763b69dc431ed35
andradebyte <joaoigorandrade21052004@gmail.com>
2025-07-25 13:19:06 -0300

    b

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import verificacaoDadosRoutes from "./routes/verificacaoDadosRoutes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 9323;
 
